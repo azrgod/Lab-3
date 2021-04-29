f0bed2aaf189d209b0c80ec8fc6167de7e0c5834
azrgod <azrgod>
2021-04-29 10:51:14 -0600

    first commit

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void buttonGuardar_Clicked(object sender, EventArgs e)
+        {
+            var label = new Label { Text = Conductor.Text, BackgroundColor = Color.White, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TextColor = Color.Black, FontSize = 50, };
+            var label1 = new Label { Text = Marca.Text, BackgroundColor = Color.White, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TextColor = Color.Black, FontSize = 50, };
+            var label2 = new Label { Text = Modelo.Text, BackgroundColor = Color.White, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TextColor = Color.Black, FontSize = 50, };
+            Parcial.Children.Add(label);
+            Parcial.Children.Add(label1);
+            Parcial.Children.Add(label2);
+        }
+    }
+}
